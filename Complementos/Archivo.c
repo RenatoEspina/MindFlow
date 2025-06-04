c7d7f7c0d006f857303f5b026a4a062e007ef7fb
RenatoEspina <renato.espina.e@mail.pucv.cl>
2025-06-03 21:38:04 -0400

    funcion para la creacion de los .csv en sus respectivas carpetas creada

--- Complementos/Archivo.c
+++ Complementos/Archivo.c
@@ -1,22 +1,37 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <direct.h>
 
 void comprobarExistenciaArchivo() {
     const char* rutas[] = {
-        "../Usuario/Cursos/cursos.csv",
-        "../Usuario/Cursos/notas.csv",
-        "../Usuario/Cursos/repasos.csv",
-        "../Usuario/Cursos/preguntas.csv",
-        "../Usuario/Calendario/agenda.csv"
+        "Usuario/Cursos/cursos.csv",
+        "Usuario/Cursos/notas.csv",
+        "Usuario/Cursos/repasos.csv",
+        "Usuario/Cursos/preguntas.csv",
+        "Usuario/Calendario/agenda.csv"
     };
 
+    const char *encabezados[] = {
+        "idCurso,nombre",
+        "idCurso,nota,ponderacion",
+        "idCurso,idRepaso,promedio,promedioAnterior",
+        "idCurso,idRepaso,pregunta,respuesta,puntuacion",
+        "dia,mes,nombre,descripcion,estado"
+    };
+
+    // Revisa si existe la carpeta con el nombre, si no lo crea
+    _mkdir("Usuario");
+    _mkdir("Usuario/Cursos");
+    _mkdir("Usuario/Calendario");
+
+    // Revisa si existen los archivos .csv, si no existen, los crea con sus respectivos encabezados
     for (int i = 0; i < 5; i++) {
         FILE* f = fopen(rutas[i], "r");
         if (!f) {
-            f = fopen(rutas[i], "w"); // Crea el archivo vacío si no existe
+            f = fopen(rutas[i], "w");
+            fprintf(f, "%s\n", encabezados[i]);
             if (f) fclose(f);
         } else {
             fclose(f);
         }
     }
 }
-
